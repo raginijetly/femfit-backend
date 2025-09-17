47d7fd041ff4dbee0b20399960546d1103136aa5
raginijetly <raginijetly98@gmail.com>
2025-09-17 23:44:46 +0530

    Testing fix for nodemailer not working

--- service/nodeMailer.js
+++ service/nodeMailer.js
@@ -2,17 +2,20 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_APP_MAIL,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  connectionTimeout: 20 * 1000, // 20 secs
 });
 
 const sendMail = async ({ to, subject, html }) => {
   try {
     await transporter.sendMail({
-      from: "FemFit <no-reply@femfit.com>",
+      from: `"FemFit" <${process.env.GMAIL_APP_MAIL}>`,
       to,
       subject,
       html: html,
